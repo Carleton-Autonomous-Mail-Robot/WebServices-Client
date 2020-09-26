f8d80d4d280d5863325b033e6d95f3b06e6d9c68
Gabriel Ciolac <gabrielciolac@gmail.com>
2020-09-25 23:21:59 -0400

    Removed Violet UML, added some commenting

--- client_controller.py
+++ client_controller.py
@@ -1,32 +1,85 @@
-"""
-This class is used for the Client in
-the server, client pair.
 
-"""
 from cipher import Cipher
 from flask import jsonify
 from client_server_interface import Interface
 import json
 
-class Controller:
+"""
+This class is used for the Client in
+the server-client pair.
+
+"""
+class Client_Controller:
+
+    '''
+        Constructor for Client_Controller
+        @Author Gabriel Ciolac
+    '''
     def __init__(self):
         self.__cipher = Cipher()
         self.__clientID = self.__loadClientID()
         self.__serverPublicKey = ""
         self._interface = Interface()
 
+    '''
+        Loads ClientID from a configuration file
+        @Author Gabriel Ciolac
+    '''
     def __loadClientID(self):
         return "TEST ID"
 
+
+    '''
+        Forwards a message to the client-server interface.
+        Takes the response from server and turns it into a JSON.
+        Sends that JSON to the controller function which determins
+        what todo with the response.
+
+        Returns True if client was able to process response.
+        Returns False if response was unable to be processed.
+
+        @Author Gabriel Ciolac
+    '''
     def send(self,msg):
-        self._interface.send(status='good',id=self.__clientID,msg='test')
-    
+        res = self._interface.send(status='good',id=self.__clientID,msg='test')
+        try:
+            payload = json.load(res)
+        except:
+            return False
+        return self.__controller(payload)
+        
+
+    '''
+        Decides what todo next with a server response depending
+        on status.
+
+        @Author Gabriel Ciolac
+    '''
+    def __controller(self,payload):
+        if payload['status'] is 'done':
+            return True
+        elif payload['status'] is 'good':
+            return self.__recieve(payload['package'])
+        elif payload['status'] is 'configure':
+            return self.__configure()
+        return False
+        
+    '''
+        Negotiates a key exhange with the server
+        @Author Gabriel Ciolac
+    '''
     def __configure(self):
-        pass
+        return True
 
+    '''
+        Decrypts payload using private key
+        
+        @Author Gabriel Ciolac
+    '''
     def __recieve(self,msg):
-        pass
+        return True
+        
 
 
-control = Controller()
+control = Client_Controller()
 control.send('test')
--- client_server_interface.py
+++ client_server_interface.py
@@ -1,15 +1,25 @@
 import requests
 
+'''
+	Interface class is the border of the system
 
+	Maybe refactored out in the future, and incorporated into client_controller
+
+	@Author Gabriel Ciolac
+'''
 class Interface:
+
 
 	def __init__(self):
 		self._tmp = ''
-
+	'''
+		Forwards a message to server
+		@Author Gabriel Ciolac
+	'''
 	def send(self,status,msg,id):
 		res = requests.post('http://127.0.0.1:5000/inbox', data = {'status':status,'ID':id, 'msg':msg})
 
-		print(res.text)
+		return res.txt
 
 
 
